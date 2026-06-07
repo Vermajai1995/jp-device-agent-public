055616760d739f4d77df14eba7c7f474ee42fabf
Jai Patel <48493248+Vermajai1995@users.noreply.github.com>
2026-06-07 11:42:08 +0000

    Add installers

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$InstallDir = "$env:ProgramData\JPDeviceAgent"
+$InstallDir = "$env:USERPROFILE\jp-device-agent"
 
 Write-Host ""
 Write-Host "Installing JP Device Agent..."
@@ -6,16 +6,14 @@ Write-Host ""
 
 New-Item -ItemType Directory -Force -Path $InstallDir | Out-Null
 
-$ZipFile = "$InstallDir\jp-device-agent.zip"
-
 Invoke-WebRequest `
-  -Uri "https://github.com/Vermajai1995/jp-device-agent/releases/latest/download/jp-device-agent.zip" `
-  -OutFile $ZipFile
+-Uri "https://github.com/Vermajai1995/jp-device-agent/releases/latest/download/jp-device-agent.zip" `
+-OutFile "$InstallDir\jp-device-agent.zip"
 
 Expand-Archive `
-  -Path $ZipFile `
-  -DestinationPath $InstallDir `
-  -Force
+-Path "$InstallDir\jp-device-agent.zip" `
+-DestinationPath $InstallDir `
+-Force
 
 Write-Host ""
 Write-Host "Installed at:"
